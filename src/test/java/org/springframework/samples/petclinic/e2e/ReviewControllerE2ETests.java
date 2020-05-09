e32720914b6d11ae5dcfd5f0b034b480e30190ba
Ian Spektor <ianspektor10@gmail.com>
2020-05-09 16:57:56 +0200

    Pruebas con MySQL pasan, pero con BD en memoria no

--- src/test/java/org/springframework/samples/petclinic/e2e/ReviewControllerE2ETests.java
+++ src/test/java/org/springframework/samples/petclinic/e2e/ReviewControllerE2ETests.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @Transactional
+@DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class ReviewControllerE2ETests {
 
 	public final String TEST_OWNER_USERNAME = "fede"; //Se crea al levantar la BD en memoria con data.sql
